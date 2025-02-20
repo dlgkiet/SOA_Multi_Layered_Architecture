81e7d4a27338c10058b41f3793a6b806a30438d3
Hiu11 <Tronghieudo2k4@gmail.com>
2025-02-20 14:58:52 +0700

    update

--- SOA_Layered_Arch/SOA_Layered_Arch/CoreLayer/Entities/Review.cs
+++ SOA_Layered_Arch/SOA_Layered_Arch/CoreLayer/Entities/Review.cs
@@ -1,6 +1,12 @@
 ﻿namespace SOA_Layered_Arch.CoreLayer.Entities
 {
-    public class Review
-    {
-    }
+   
+        public class Review
+        {
+            public int Id { get; set; }
+            public int MovieId { get; set; }
+            public int UserId { get; set; }
+            public string ReviewText { get; set; }
+            public DateTime ReviewDate { get; set; }
+        }
 }
--- SOA_Layered_Arch/SOA_Layered_Arch/DataAccessLayer/Repositories.cs
+++ SOA_Layered_Arch/SOA_Layered_Arch/DataAccessLayer/Repositories.cs
@@ -1,6 +1,30 @@
-﻿namespace SOA_Layered_Arch.DataAccessLayer
+﻿using Microsoft.EntityFrameworkCore;
+using SOA_Layered_Arch.CoreLayer.Entities;
+
+namespace SOA_Layered_Arch.DataAccessLayer
 {
     public class Repositories
     {
+        private readonly AppDbContext _context;
+
+        // Constructor để khởi tạo _context
+        public Repositories(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Phương thức CRUD
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // Phương thức gọi Stored Procedure
+        public async Task<IEnumerable<Movie>> GetTopRatedMoviesWithSpAsync(int topCount)
+        {
+            return await _context.Movies
+                .FromSqlRaw("EXEC GetTopRatedMovies @top_count = {0}", topCount)
+                .ToListAsync();
+        }
     }
 }
