fcc7699aeccef86e44e77ed8f457a66dbc438b6e
thanhlamcode <lamdoan1122334455@gmail.com>
2025-02-20 15:08:52 +0700

    add Bussiness and Service Layer

--- SOA_Layered_Arch/SOA_Layered_Arch/BusinessLayer/RatingCalculator.cs
+++ SOA_Layered_Arch/SOA_Layered_Arch/BusinessLayer/RatingCalculator.cs
@@ -1,6 +1,13 @@
 ﻿namespace SOA_Layered_Arch.BusinessLayer
 {
-    public class RatingCalculator
+    public static class RatingCalculator
     {
+        public static decimal CalculateAverageRating(IEnumerable<Rating>
+       ratings)
+        {
+            if (ratings == null || !ratings.Any())
+                return 0;
+            return ratings.Average(r => r.Value);
+        }
     }
 }
---
+++ SOA_Layered_Arch/SOA_Layered_Arch/ServiceLayer/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using SOA_Layered_Arch.CoreLayer.Entities;
+using SOA_Layered_Arch.DataAccessLayer.Repositories;
+
+
+namespace SOA_Layered_Arch.ServiceLayer
+{
+    public class MovieService
+    {
+        private readonly MovieRepository _movierepositories; // Sử dụng class Repositories thay vì Interface
+
+        // Constructor - Dependency Injection
+        public MovieService(MovieRepository repositories)
+        {
+            _movierepositories = repositories;
+        }
+
+        // Lấy danh sách tất cả phim
+        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
+        {
+            return await _movierepositories.GetAllMoviesAsync();
+        }
+
+        // Lấy danh sách phim có đánh giá cao nhất (Stored Procedure)
+        public async Task<IEnumerable<Movie>> GetTopRatedMoviesWithSpAsync(int topCount)
+        {
+            return await _movierepositories.GetTopRatedMoviesWithSpAsync(topCount);
+        }
+    }
+}
